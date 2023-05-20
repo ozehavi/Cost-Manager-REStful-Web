d54c6cdb4d63146e36fcb61108bff53f7d266f76
ozehavi <ozehavi@gmail.com>
2023-05-20 20:23:31 +0300

    Add about endpoint and create an initial addcost form

--- routes/addcost.js
+++ routes/addcost.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET addcost page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'addcost' });
+  res.render('addcost', { title: 'addcost' });
 });
 
 module.exports = router;
