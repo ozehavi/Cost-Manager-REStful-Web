e96f9a2656414309a0b7bd5b43e4de022c487ba8
ozehavi <ozehavi@gmail.com>
2023-05-20 15:55:01 +0300

    create endpoints

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const addCostRouter = require('./routes/addcost');
+const reportRouter = require('./routes/report');
+const aboutRouter = require('./routes/about');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/addcost', addCostRouter);
+app.use('/report', reportRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'index' });
+    res.render('index', { title: 'about' });
 });
 
 module.exports = router;
